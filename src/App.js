aad75c371c07032328e4ac993aa3093aa11ccec2
Lijomon C John <lijomon@promasoft.net>
2020-12-14 18:04:59 +0530

    Complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,152 @@
+import React, { Component } from 'react'
+import ReactCrop from 'react-image-crop';
+import 'react-image-crop/dist/ReactCrop.css';
+
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Container } from 'reactstrap';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: false,
+      src: null,
+      crop: {
+        unit: '%',
+        width: 30,
+        aspect: 1 / 1 // You can change aspect ratio of cropped imahe here
+      },
+    }
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle = () => {
+    this.setState({
+      src: null,
+      modal: !this.state.modal
+    })
+    console.log(this.state.modal);
+  }
+
+  // Load image to Cropper
+  onSelectFile = e => {
+    if (e.target.files && e.target.files.length > 0) {
+      const reader = new FileReader();
+      reader.addEventListener('load', () =>
+        this.setState({ src: reader.result })
+      );
+      reader.readAsDataURL(e.target.files[0]);
+    }
+  };
+
+  // If you setState the crop in here you should return false.
+  onImageLoaded = image => {
+    this.imageRef = image;
+  };
+
+  onCropComplete = crop => {
+    this.makeClientCrop(crop);
+  };
+
+  onCropChange = (crop, percentCrop) => {
+    // You could also use percentCrop:
+    // this.setState({ crop: percentCrop });
+    this.setState({ crop });
+  };
+
+  async makeClientCrop(crop) {
+    if (this.imageRef && crop.width && crop.height) {
+      const croppedImageUrl = await this.getCroppedImg(
+        this.imageRef,
+        crop,
+        'newFile.jpeg'
+      );
+      this.setState({ croppedImageUrl });
+    }
+  }
+
+  getCroppedImg(image, crop, fileName) {
+    const canvas = document.createElement('canvas');
+    const scaleX = image.naturalWidth / image.width;
+    const scaleY = image.naturalHeight / image.height;
+    canvas.width = crop.width;
+    canvas.height = crop.height;
+    const ctx = canvas.getContext('2d');
+
+    ctx.drawImage(
+      image,
+      crop.x * scaleX,
+      crop.y * scaleY,
+      crop.width * scaleX,
+      crop.height * scaleY,
+      0,
+      0,
+      crop.width,
+      crop.height
+    );
+
+    return new Promise((resolve, reject) => {
+      canvas.toBlob(blob => {
+        if (!blob) {
+          //reject(new Error('Canvas is empty'));
+          console.error('Canvas is empty');
+          return;
+        }
+        blob.name = fileName;
+        window.URL.revokeObjectURL(this.fileUrl);
+        this.fileUrl = window.URL.createObjectURL(blob);
+        resolve(this.fileUrl);
+      }, 'image/jpeg');
+    });
+  }
+
+  render() {
+    const { crop, croppedImageUrl, src } = this.state;
+
+    return (
+      <Container className="themed-container" style={{ padding: 10 }}>
+        <div>
+          {croppedImageUrl && (
+            <img alt="Crop" style={{ height: 220, width: 220 }} src={croppedImageUrl} />
+          )}
+          {!croppedImageUrl && (
+            <img alt="Crop" style={{ height: 220, width: 220 }} src={logo} />
+          )}
+        </div>
+        <div>
+          <Button className="mt-5" color="danger" onClick={this.toggle}>Upload Image</Button>
+        </div>
+        <Modal isOpen={this.state.modal} toggle={this.toggle}>
+          <ModalHeader toggle={this.toggle}>Crop image</ModalHeader>
+          <ModalBody>
+            <div className="App">
+              <div>
+                <input type="file" accept="image/*" onChange={this.onSelectFile} />
+              </div>
+              {src && (
+                <ReactCrop
+                  src={src}
+                  crop={crop}
+                  ruleOfThirds
+                  onImageLoaded={this.onImageLoaded}
+                  onComplete={this.onCropComplete}
+                  onChange={this.onCropChange}
+                />
+              )}
+
+            </div>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={this.toggle}>Cancel</Button>{' '}
+            <Button color="secondary" onClick={this.toggle}>Done</Button>
+          </ModalFooter>
+        </Modal>
+      </Container>
+    );
+  }
 }
 
 export default App;
+
+
